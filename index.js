02cd949e6f0fe00ed6c7730f4fbfa4b75d337bab
Michael Armes <michael@un-loop.org>
2019-07-19 09:46:27 -0700

    remove dependency on koa app, compose middleware for routes

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const static = require("koa-static")
-const router = require("koa-router")
+const static = require("koa-static");
+const router = require("koa-router");
+const compose = require("koa-compose");
+
 const checkPermission = require("unloop-check-permission");
 
-module.exports = (path, routes, loginPath) => (app, scheme) => {
+module.exports = (path, routes, loginPath) => (scheme) => {
     loginPath = loginPath ? loginPath : '/login';
     scheme = scheme ? scheme : 'roles';
-
+    let routeMiddleware;
 
     if (routes) {
         const authRouter = new router();
 
         for(let route of routes) {
             if (!route || !route.route) continue;
+            if (!route.middleware) route.middleware = [];
 
-            authRouter.all(route.route, async (ctx, next) =>
-            {
-                if (!checkPermission[scheme](ctx, route.permissions)) {
-                    ctx.redirect(`${loginPath}?redirect=${encodeURIComponent(ctx.url)}`);
-                } else {
-                    await next();
-                }
-            });
+            authRouter.all(route.route, compose([...route.middleware, async (ctx, next) =>
+                {
+                    if (!checkPermission[scheme](ctx, route.permissions)) {
+                        ctx.redirect(`${loginPath}?redirect=${encodeURIComponent(ctx.url)}`);
+                    } else {
+                        await next();
+                    }
+                }])
+            );
         }
-        app.use(authRouter.middleware());
+        
+        routeMiddleware = authRouter.middleware();
     }
 
-    return static(path);
+    return routeMiddleware ? compose([routeMiddleware, static(path)]) : static(path);
 }
