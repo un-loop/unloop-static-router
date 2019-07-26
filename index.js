32a368cc6bcbff052db7b9722f70e8ee2c853ae2
Michael Armes <michael@un-loop.org>
2019-07-26 14:11:10 -0700

    udpate API

--- index.js
+++ index.js
@@ -4,31 +4,47 @@ const compose = require("koa-compose");
 
 const checkPermission = require("unloop-check-permission");
 
-module.exports = (path, routes, loginPath) => (scheme) => {
+ module.exports = ({root, routes, loginPath, scheme}) => {
     loginPath = loginPath ? loginPath : '/login';
     scheme = scheme ? scheme : 'roles';
-    let routeMiddleware;
 
-    if (routes) {
-        const authRouter = new router();
-
-        for(let route of routes) {
-            if (!route || !route.route) continue;
-            if (!route.middleware) route.middleware = [];
-
-            authRouter.all(route.route, compose([...route.middleware, async (ctx, next) =>
-                {
-                    if (!checkPermission[scheme](ctx, route.permissions)) {
-                        ctx.redirect(`${loginPath}?redirect=${encodeURIComponent(ctx.url)}`);
-                    } else {
-                        await next();
-                    }
-                }])
-            );
+    const used = [];
+    this.use = (middleware) => {
+        if (typeof middleware !== "function") {
+            throw new Error("middleware must be a function");
         }
-        
-        routeMiddleware = authRouter.middleware();
+
+        used.push(middleware);
     }
 
-    return routeMiddleware ? compose([routeMiddleware, static(path)]) : static(path);
+    this.middleware = (scheme) =>
+    {
+        let routeMiddleware;
+
+        if (routes) {
+            const authRouter = new router();
+
+            for(let route of routes) {
+                if (!route || !route.route) continue;
+                if (!route.middleware) route.middleware = [];
+
+                authRouter.all(route.route, compose([...route.middleware, async (ctx, next) =>
+                    {
+                        if (!checkPermission[scheme](ctx, route.permissions)) {
+                            ctx.redirect(`${loginPath}?redirect=${encodeURIComponent(ctx.url)}`);
+                        } else {
+                            await next();
+                        }
+                    }])
+                );
+            }
+            
+            routeMiddleware = authRouter.middleware();
+        }
+
+        return routeMiddleware ? compose([routeMiddleware, used, static(root)]) : static(root);
+    }
+
+
+
 }
